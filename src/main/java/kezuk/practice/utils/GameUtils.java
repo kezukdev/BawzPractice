9303a157c6e016b2ba7a7e25e230e685f1374966
» Kezuk <54121447+kezukdev@users.noreply.github.com>
2022-11-10 23:08:09 +0100

    fix bug of invisible opponent

--- src/main/java/kezuk/practice/utils/GameUtils.java
+++ src/main/java/kezuk/practice/utils/GameUtils.java
@@ -65,6 +65,7 @@ public class GameUtils {
             			}	
             		}
                 }
+                player.showPlayer(Bukkit.getPlayer(GameUtils.getOpponent(player.getUniqueId())));
 			}
 		}
 	}
